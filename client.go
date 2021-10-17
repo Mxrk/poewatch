4bee9e3df5a53abde108621e67fc46f0373ebc15
Mark :) <Mxrk@users.noreply.github.com>
2021-10-17 17:36:40 +0200

    feat: add corruptions endpoint

--- client.go
+++ client.go
@@ -97,3 +97,11 @@ func GetItemHistory(itemID, league string) History {
 	json.Unmarshal([]byte(req), &st)
 	return st
 }
+
+//GetCorruptions returns corruption data for a given item
+func GetCorruptions(itemID, league string) []Corruptions {
+	st := []Corruptions{}
+	req := get(baseURL + "corruptions?id=" + itemID + "&league=" + league)
+	json.Unmarshal([]byte(req), &st)
+	return st
+}
